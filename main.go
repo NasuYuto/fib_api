199a48223ba5817c8c1d5df2702881c44352f44f
なゆとも <nayutomo@nayutomononotobukkukonpyuta.local>
2024-11-17 16:46:36 +0900

    port 8000　→ 80

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	http.HandleFunc("/fib", handlers.FibonacciHandler)
 
-	log.Println("Starting server on :8000")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	log.Println("Starting server on :80")
+	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
 }
